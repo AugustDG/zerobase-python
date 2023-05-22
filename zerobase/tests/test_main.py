a9c8f1e98c7c90a66b04be1e3605e3bd30b05b20
Augusto Mota Pinheiro <augustomp55@gmail.com>
2023-05-22 17:29:56 -0400

    [Feature] Added possibility of multiple subscriptions
    Updated the main_test script to reflect changes.

--- zerobase/tests/test_main.py
+++ zerobase/tests/test_main.py
@@ -1,22 +1,31 @@
 
 import random
 import time
-from zerobase.zerobase import ZeroBase
+from zerobase.zerobase import ZeroBase, ZeroBasePubConfig, ZeroBaseSubConfig
 
 def main() -> bool:
     print("Main loop running...")
 
     msg = str((random.randint(1, 100), random.randint(1, 100)))
-    print("Sending message: " + msg)
-    base.send(msg)
+    base.send(msg, "A")
 
-    time.sleep(random.randint(1, 5))
+    msg = str((random.randint(1, 100), random.randint(1, 100)))
+    base.send(msg, "B")
+
+    msg = str((random.randint(1, 100), random.randint(1, 100)))
+    base.send(msg, "C")
+
+    time.sleep(random.randint(1, 3))
 
     return True
 
 def on_msg_received(topic, msg):
-    print("Message received: " + msg + " on topic: " + topic)
+    print("Message received: " + msg + " on topic: \"" + topic + "\"")
 
 if __name__ == "__main__":
-    base = ZeroBase(send_addr="tcp://*:5556", recv_addr="tcp://localhost:5555", main=main, msg_received=on_msg_received)
+    pub_config = ZeroBasePubConfig(addr="tcp://*:5555")
+    sub_config_1 = ZeroBaseSubConfig(addr="tcp://localhost:5555", topics=["A"])
+    sub_config_2 = ZeroBaseSubConfig(addr="tcp://localhost:5555", topics=["B"])
+
+    base = ZeroBase(pub_config=pub_config, sub_configs=[sub_config_1, sub_config_2], main=main, msg_received=on_msg_received)
     base.run()
--- zerobase/zerobase.py
+++ zerobase/zerobase.py
@@ -9,23 +9,60 @@ Not licensed under any license. All rights reserved.
 
 import pickle
 import sys
-from typing import Any, Callable
-
 import threading
+from attr import dataclass
 import zmq
 import signal
 
+from typing import Any, Callable, List
+
+@dataclass
+class ZeroBasePubConfig:
+    """
+    This represents the configuration for a ZeroBase publisher socket.
+    """
+
+    addr: str
+
+@dataclass
+class ZeroBaseSubConfig:
+    """
+    This represents the configuration for a ZeroBase subscriber socket.
+    """
+
+    addr: str
+    topics: List[str]
+
+@dataclass
+class ZeroBasePubSocket:
+    """
+    This represents a ZeroBase publisher socket.
+    """
+
+    socket: zmq.Socket
+    config: ZeroBasePubConfig
+
+@dataclass
+class ZeroBaseSubSocket:
+    """
+    This represents a ZeroBase subscriber socket.
+    """
+
+    socket: zmq.Socket
+    config: ZeroBaseSubConfig
+    registered: bool = False
+
 class ZeroBase():
     """
-    This class is the base class for all ZeroMQ-based programs for NanoStride. It handles all of the necessary setup and teardown for ZeroMQ, and provides a simple interface for sending and receiving messages.
+    This is the base class for all ZeroMQ-based programs for NanoStride. It handles all of the necessary setup and teardown for ZeroMQ, and provides a simple interface for sending and receiving messages.
     """
     
-    def __init__(self, send_addr: str | None = None, recv_addr: str | None = None, send_topic: str = "", recv_topic: str = "", main: Callable[[], bool] | None = None, msg_received: Callable[[str, Any], None] | None = None, logger: Callable[[Any], None] = print) -> None:
+    def __init__(self, pub_config: ZeroBasePubConfig | None, sub_configs: List[ZeroBaseSubConfig] = [], main: Callable[[], bool] | None = None, msg_received: Callable[[str, Any], None] | None = None, logger: Callable[[Any], None] = print) -> None:
         signal.signal(signal.SIGINT, self._signal_handler)
 
-        # assign topic properties
-        self.send_topic = send_topic
-        self.recv_topic = recv_topic
+        # assign socket configurations
+        self.pub_config = pub_config
+        self.sub_configs = sub_configs
 
         # assign callback properties
         self._main = main
@@ -34,20 +71,26 @@ class ZeroBase():
 
         # initialize ZMQ properties
         self._ctx = zmq.Context()
-        self._send_socket = None
-        self._recv_socket = None
+        self.pub_sockets: List[ZeroBasePubSocket] = []
+        self.sub_sockets: List[ZeroBaseSubSocket] = []
     
         # initialize necessary sockets
-        if send_addr is not None:
-            self._send_socket = self._ctx.socket(zmq.PUB)
-            self._send_socket.bind(send_addr)
+        if self.pub_config is not None:
+            socket = self._ctx.socket(zmq.PUB)
+            socket.bind(self.pub_config.addr)
 
-        if recv_addr is not None:
+            self.pub_socket = ZeroBasePubSocket(socket, self.pub_config)
+
+        for config in self.sub_configs:
             self.comms_can_run = True
 
-            self._recv_socket = self._ctx.socket(zmq.SUB)
-            self._recv_socket.connect(recv_addr)
-            self._recv_socket.setsockopt_string(zmq.SUBSCRIBE, recv_topic)
+            socket = self._ctx.socket(zmq.SUB)
+            socket.connect(config.addr)
+
+            for topic in config.topics:
+                socket.setsockopt_string(zmq.SUBSCRIBE, topic)
+            
+            self.sub_sockets.append(ZeroBaseSubSocket(socket, config))
 
 
     def run(self) -> None:
@@ -74,6 +117,8 @@ class ZeroBase():
         """
         Starts this instance's necessary threads. Must be called before sending or receiving messages.
         """
+
+        self._logger("Starting ZeroBase...")
         
         # start the receive loop thread
         self._receive_loop_thread = threading.Thread(target=self._receive_loop)
@@ -94,31 +139,69 @@ class ZeroBase():
 
         self._ctx.destroy()
         
-    def send(self, msg: Any, topic: str | None = None) -> None:
+    def send(self, msg: Any, topic: str) -> None:
         """
-        Sends a message to the specified topic. If no topic is specified, the default topic for this instance will be used.
+        Sends a message to the specified topic.
         """
 
+        self._logger("Sending message " + str(msg) + " on topic: \"" + topic + "\"")
+
         # send the message if the socket has been opened
-        if self._send_socket is not None:
-            self._send_socket.send(bytes(self.send_topic if topic is None else topic, "utf-8"), zmq.SNDMORE)
-            self._send_socket.send_pyobj(msg)
+        if self.pub_socket is not None:
+            self.pub_socket.socket.send(bytes(topic, "utf-8"), zmq.SNDMORE)
+            self.pub_socket.socket.send_pyobj(msg)
 
     # receive a message from the specified topic
     def _receive_loop(self) -> None:
+        poller = zmq.Poller()
+
+        self._logger("Registering sockets...")
+
+        for sub_socket in self.sub_sockets:
+            self._logger("Registering sub socket on address " + sub_socket.config.addr + " with topics: " + str(sub_socket.config.topics))
+            
+            poller.register(sub_socket.socket, zmq.POLLIN)
+            sub_socket.registered = True
+        
+        registered_qty = len(self.sub_sockets)
+
+        self._logger("Registered sockets: " + str(registered_qty))
+        self._logger("ZeroBase receive loop started!")
+
         # run the comms loop
         while self.comms_can_run:
-            # check if the socket has been opened (otherwise, there's no point in trying to receive messages)
-            if self._recv_socket is None:
+            # check if any sockets have been registered (otherwise, there's no point in trying to receive messages)
+            if len(self.sub_sockets) == 0:
                 continue
 
-            # tries to manually deserialize the received message (because the first frame is the topic)
+            # register any new sockets
+            if registered_qty <= len(self.sub_sockets):
+                for sub_socket in filter(lambda socket: (not socket.registered), self.sub_sockets):
+                    poller.register(sub_socket.socket, zmq.POLLIN)
+                    sub_socket.registered = True
+                
+                registered_qty = len(self.sub_sockets)
+
+            # poll for any messages
             try:
-                recv_msg = self._recv_socket.recv_multipart()
+                ready_sockets = dict(poller.poll())
+                
+                self._process_poll(ready_sockets)
             except:
                 # if the message can't be received, just ignore it
                 continue
 
+    # processes the poll results
+    def _process_poll(self, ready_sockets: dict[Any, int]) -> None:
+        for sub_socket in self.sub_sockets:
+            # not super sure why I can't use the socket that was returned by the poller, but it doesn't work for some reason
+            # this is just following the example from the ZeroMQ guide
+            if sub_socket.socket not in ready_sockets:
+                continue
+
+            recv_msg = sub_socket.socket.recv_multipart()
+
+            # tries to manually deserialize the received message (because the first frame is the topic)
             # the first frame is the topic, and the second is the message
             recv_topic = recv_msg.pop(0).decode("utf-8")
             recv_obj = recv_msg.pop()
@@ -128,8 +211,8 @@ class ZeroBase():
             if self._msg_received is not None:
                 self._msg_received(recv_topic, recv_obj)
 
+
     # handle OS signals
     def _signal_handler(self, sig, frame) -> None:
         self.stop()
         sys.exit(sig)
-
